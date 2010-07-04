bf8c23e262a791d1bd68e0814cf2d9a48bc3f751
Christopher M. Judd <github@juddsolutions.com>
2010-07-04 13:22:45 -0400

    Updated TruthKoan to make it more usable for learning Groovy truth and to set a pattern going forward.

--- src/koans/TruthKoans.groovy
+++ src/koans/TruthKoans.groovy
@@ -8,58 +8,81 @@ package koans;
 *
 */
 class TruthKoans extends Koans {
+	public static ZERO = 0
+	public static NON_ZERO = 3
+	public static EMPTY_STRING = ""
+	public static NEGATIVE_NUMBER = -1
 
-	void testTruth() {
-		assert ___
+	// Boolean expressions
+	
+	void testTrueForTruth() {
+		assertEquals __, true
 	}
 	
-	void testFalse() {
-		assert !__
+	void testFalseForTruth() {
+		assertEquals __, false
 	}
 	
-	void testNumberZeroIsFalse() {
-		def zero = 0
-		assert !__
-	}
+	// Objects
 	
-	void testNonZeroNumberIsTrue() {
-		def nonZero = 3
-		assert ___
-	}
-	
-	void testEmptyStringIsFalse() {
-		def str = ""
-		assert !__
-	}
-	
-	void testNonEmptyStringIsTrue() {
-		def str = "Groovy Is Cool!"
-		assert ___
-	}
-	
-	void testEmptyCollectionIsFalse() {
-		def col = []
-		assert !__
-	}
-	
-	void testCollectionWithItemsIsTrue() {
-		def col = [1,2,3]
-		assert ___
-	}
-	
-	void testIteratorOnEmptyCollectionIsFalse() {
-		def iter = [].iterator()
-		assert !__
-	}
-	
-	void testIteratorOnNonEmptyCollectionIsTrue() {
-		def iter = [1,2,3].iterator()
-		assert ___
-	}
-	
-	void testObjectReferencesIsTrue() {
-		def object = new Object()
-		assert ___
+	void testNullForTruth() {
+		assertEquals __, null
 	}
 
+	void testObjectReferencesForTruth() {
+		assertEquals __, new Object()
+	}
+	
+	// Numbers
+	
+	void testZeroForTruth() {
+		assertEquals __, ZERO
+	}
+	
+	void testNonZeroForTruth() {
+		assertEquals __, NON_ZERO
+	}
+	
+	void testNegativeNumberForTruth() {
+		assertEquals __, NEGATIVE_NUMBER
+	}
+	
+	// Strings
+	
+	void testEmptyStringForTruth() {
+		assertEquals __, EMPTY_STRING
+	}
+	
+	void testNonEmptyForTruth() {
+		assertEquals __, "Groovy is cool!!!"
+	}
+	
+	// Collections
+	
+	void testEmptyCollectionForTruth() {
+		assertEquals __, []
+	}
+	
+	void testCollectionWithItemsForTruth() {
+		assertEquals __, [1,2,3] 
+	}
+	
+	void testIteratorOnEmptyCollectionForTruth() {
+		assertEquals __, [].iterator()
+	}
+	
+	void testIteratorOnNonEmptyForTruth() {
+		assertEquals __, [1,2,3].iterator()
+	}
+	
+	// Maps
+	
+	void testEmptyMapForTruth() {
+		assertEquals __, [:]
+	}
+	
+	void testPopulatedMapForTruth() {
+		assertEquals __, ['one':1]
+	}
+	
 }
