4aea70ba54129bf8833fe0c1af71b462210dd885
Richard Paul <richard.a.paul@gmail.com>
2010-11-02 22:16:08 +0000

    Cleaned up indentation to consistently use tabs to enhance readability

--- koans/RangesKoans.groovy
+++ koans/RangesKoans.groovy
@@ -6,48 +6,48 @@ class RangesKoans extends Koans {
 		def range = 1..5
 	
 		assert __ instanceof Range
-	  }
+	}
 	
-	  void testRangeIsAlsoList() {
+	void testRangeIsAlsoList() {
 		def range = 1..5
 	
 		assert __ instanceof List
-	  }
+	}
 	
-	  void testRangeSize() {
+	void testRangeSize() {
 		def range = 1..5
 	
 		assert __ == range.size()
-	  }
+	}
 	
-	  void testRangeAccessors() {
+	void testRangeAccessors() {
 		def range = 1..5
 	
 		assert range.get(2) == __
 		assert range[2] == __
-	  }
+	}
 	
-	  void testRangeBounds() {
+	void testRangeBounds() {
 		def range = 1..5
 	
 		assert range.from == __
 		assert range.to == __
-	  }
+	}
 	
-	  void testNonInclusiveRange() {
+	void testNonInclusiveRange() {
 		def range = 1..<5
 	
 		assert range.from == __
 		assert range.to == __
-	  }
+	}
 	
-	  void testRangesCanBeCharactersToo() {
+	void testRangesCanBeCharactersToo() {
 		def range = 'a'..'z'
 	
 		assert range instanceof Range
 		assert range.from == __
 		assert range[5] == __
 		assert range.to == __
-	  }
+	}
 
 }
